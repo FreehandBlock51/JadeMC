a351efa77db57f2dd70b6afad24d9f3ecd42f1d1
FreehandBlock51 <daltoncarter7@outlook.com>
2021-04-20 16:43:01 -0700

    Update JadeArmorMaterial.java
    Modified JadeArmorMaterial to have, for the most part, double the values of ArmorMaterial.IRON

--- src/main/java/net/dc/jademc/item/armor/JadeArmorMaterial.java
+++ src/main/java/net/dc/jademc/item/armor/JadeArmorMaterial.java
@@ -5,45 +5,58 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class JadeArmorMaterial implements IArmorMaterial {
-    @Override
-    public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
-        return 4690;
-    }
+    static final String name = "jade";
+    static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    static final int durabilityMultiplier = 30;
+    static final int[] slotProtections = new int[]{4, 10, 12, 4};
+    static final int enchantmentValue = 18;
+    static final SoundEvent sound = SoundEvents.ARMOR_EQUIP_GENERIC;
+    static final float toughness = 1.5F;
+    static final float knockbackResistance = 0.5F;
 
     @Override
-    public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
-        return 250;
-    }
-
-    @Override
-    public SoundEvent getEquipSound() {
-        return null;
-    }
-
-    @Override
-    public String getName() {
-        return "Jade";
-    }
-
-    @Override
-    public float getToughness() {
-        return 250;
-    }
-
-    @Override
-    public float getKnockbackResistance() {
-        return 250;
-    }
-
-    @Override
-    public int getEnchantmentValue() {
-        return 120;
-    }
-
-    @Override
-    public Ingredient getRepairIngredient() {
+    public int getDurabilityForSlot(EquipmentSlotType type) {
+        return HEALTH_PER_SLOT[type.getIndex()] * durabilityMultiplier;
+     }
+  
+     @Override
+     public int getDefenseForSlot(EquipmentSlotType type) {
+        return slotProtections[type.getIndex()];
+     }
+  
+     @Override
+     public int getEnchantmentValue() {
+        return enchantmentValue;
+     }
+  
+     @Override
+     public SoundEvent getEquipSound() {
+        return sound;
+     }
+  
+     @Override
+     public Ingredient getRepairIngredient() {
         return Ingredient.of(Archive.JADE.getDefaultInstance());
-    }
+     }
+  
+     @Override
+     @OnlyIn(Dist.CLIENT)
+     public String getName() {
+        return name;
+     }
+  
+     @Override
+     public float getToughness() {
+        return toughness;
+     }
+  
+     @Override
+     public float getKnockbackResistance() {
+        return knockbackResistance;
+     }
 }
