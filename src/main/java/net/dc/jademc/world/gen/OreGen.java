dd7fbf91df1e91d377af27f32098416dac0121fa
FreehandBlock51 <daltoncarter7@outlook.com>
2021-04-21 14:20:04 -0700

    bosted enderite ore generation

--- src/main/java/net/dc/jademc/world/gen/OreGen.java
+++ src/main/java/net/dc/jademc/world/gen/OreGen.java
@@ -1,5 +1,8 @@
 package net.dc.jademc.world.gen;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.dc.jademc.Archive;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,20 +22,24 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 // MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, OreGen::addFeaturesToBiomes).
  
 public class OreGen {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void addFeaturesToBiomes(BiomeLoadingEvent event) {
         switch (event.getCategory()) {
             case NETHER: // the nether
-                
+                LOGGER.info("biome " + event.getName().toString() + " is in the nether");
                 break;
             case THEEND: // the end
-                addOreToBiome(GenerationStage.Decoration.LOCAL_MODIFICATIONS, event.getGeneration(), Archive.ENDERITE_ORE, new RandomBlockMatchRuleTest(Blocks.END_STONE, 0.32F), 255, 0, 5, 2);
+                LOGGER.info("biome " + event.getName().toString() + " is in the end");
+                addOreToBiome(event.getGeneration(), Archive.ENDERITE_ORE, new RandomBlockMatchRuleTest(Blocks.END_STONE, 0.32F), 255, 0, 5, 5);
                 break;
             default: // hopefully, the rest of the biomes are in the overworld :/
+                LOGGER.info("biome " + event.getName().toString() + " is in the overworld");
                 addOreToBiome(event.getGeneration(), Archive.GREEN_CRYSTAL_ORE, OreFeatureConfig.FillerBlockType.NATURAL_STONE, 1255, 3, 10, 500);
                 break;
         }
     }
- 
+
     private static void addOreToBiome(BiomeGenerationSettingsBuilder biomeGenSettings, Block oreBlock, RuleTest toReplace, int maxHeight, int minHeight, int veinSize, int veinsPerChunk) {
         addOreToBiome(GenerationStage.Decoration.UNDERGROUND_ORES, biomeGenSettings, oreBlock, toReplace, maxHeight, minHeight, veinSize, veinsPerChunk);
     }
